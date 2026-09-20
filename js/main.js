fbb3f28355819fca4a44bad38ca42b3401fb0869
SiteLineVisuals <info@sitelinevisuals3d.com>
2026-09-19 21:24:14 -0700

    Route mobile quick navigation to in-site intake

--- js/main.js
+++ js/main.js
@@ -358,7 +358,7 @@ document.addEventListener("DOMContentLoaded", function () {
         quickNav.innerHTML = [
             '<a href="packages.html">Packages</a>',
             '<a href="examples.html">Examples</a>',
-            '<a href="https://script.google.com/macros/s/AKfycbxZFA-LA6kgTBS8Hre0f7ZmTF-DIvrzijr5NN31QNaYdLXiHhJIJ-e5nPq87_2ZK53G/exec?start=1" class="project-modal-trigger">Start Project</a>'
+            '<a href="intake.html">Start Project</a>'
         ].join("");
         document.body.appendChild(quickNav);
     }
